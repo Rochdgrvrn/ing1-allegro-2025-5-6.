2913fefa20f0c94f755d64aea97b9d3298a2e3e3
Rochdgrvrn <roch.leperedegraveron@edu.ece.fr>
2025-05-18 17:09:27 +0200

    Update game.h

--- game.h
+++ game.h
@@ -24,6 +24,19 @@ extern int countdown;
 extern int frame_counter;
 extern int grid[10][10];
 extern int visited[10][10];
+extern int player_pv[4];
+extern int player_pa_turn[4];
+extern int is_alive[4];
+extern int death_order[4];
+extern int death_count;
+extern int game_over;
+extern char death_message[64];
+extern int death_message_timer;
+extern int hardcore_mode;
+extern int oneshot_timer;
+extern BITMAP *img_obstacle_layer;
+
+
 void next_turn(void);
 void show_game(void);
 void place_players_and_obstacles(void);
