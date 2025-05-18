7a2dd68bfca5e31ce6d94b352361ce16ec13e431
Rochdgrvrn <roch.leperedegraveron@edu.ece.fr>
2025-05-18 17:08:59 +0200

    Create images.h

---
+++ images.h
@@ -0,0 +1,11 @@
+#ifndef IMAGES_H
+#define IMAGES_H
+
+#include <allegro.h>
+
+extern BITMAP *img_arena;
+extern BITMAP *img_characters[4];
+extern BITMAP *buffer;
+extern BITMAP *img_obstacles;
+
+#endif
