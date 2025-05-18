6490f0d67600d2ca84d82785d9ef4f10246a5c82
Rochdgrvrn <roch.leperedegraveron@edu.ece.fr>
2025-05-18 17:05:31 +0200

    Update globals.c

--- globals.c
+++ globals.c
@@ -1,8 +1,16 @@
 #include <allegro.h>
 #include "../include/game.h"
+#include "../include/images.h"
+
+BITMAP *buffer = NULL;
+BITMAP *img_obstacle_layer = NULL;  // ✅ CE QU’IL MANQUAIT !
+BITMAP *img_attack_flash = NULL;
+BITMAP *img_fireball = NULL;
+BITMAP *img_explosion = NULL;
+BITMAP *img_heal_wave = NULL;
+
 
 GameState game_state = STATE_MENU;
-BITMAP *buffer = NULL;
 
 int current_player = 0;
 int total_players = 2;
@@ -14,5 +22,16 @@ int countdown = 20;
 int frame_counter = 0;
 int grid[10][10] = {0};
 int visited[10][10] = {0};
+int player_pv[4] = {20, 20, 20, 20};
+int player_pa_turn[4] = {6, 6, 6, 6};
+int is_alive[4] = {1, 1, 1, 1};
+int death_order[4] = {-1, -1, -1, -1};
+int death_count = 0;
+int game_over = 0;
+char death_message[64] = "";
+int death_message_timer = 0;
+int hardcore_mode = 0;
+int oneshot_timer = 0;
+
 
 
