d1eb984a8f894182a1a904e277adc928e016617b
Rochdgrvrn <roch.leperedegraveron@edu.ece.fr>
2025-05-18 16:53:38 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,10 +1,14 @@
 #include <allegro.h>
+#include "assets.h"
 #include "game.h"
 #include "menu.h"
-#include "movement.h"
 #include "utils.h"
+#include "movement.h"
+#include "spell.h"
+#include "images.h"
 
 int main() {
+    // Initialisation d'Allegro
     allegro_init();
     install_keyboard();
     install_mouse();
@@ -12,15 +16,40 @@ int main() {
     set_gfx_mode(GFX_AUTODETECT_WINDOWED, 800, 700, 0, 0);
     show_mouse(screen);
 
-    buffer = create_bitmap(800, 700);
+    // Important : gestion couleur de transparence
+    set_color_conversion(COLORCONV_TOTAL);
 
+    // Chargement des ressources visuelles
+    load_assets();
+
+    // Initialisation des classes de personnages
+    init_classes();
+
+    // Création du buffer principal
+    buffer = create_bitmap(800, 700);
+    if (!buffer) {
+        allegro_message("Erreur : buffer non créé !");
+        exit(1);
+    }
+
+    // Boucle principale
     while (!key[KEY_ESC]) {
         switch (game_state) {
-            case STATE_MENU: show_menu(); break;
-            case STATE_RULES: show_rules(); break;
-            case STATE_PLAYER_COUNT_SELECTION: show_player_count_selection(); break;
-            case STATE_CHARACTER_SELECTION: show_character_selection(); break;
-            case STATE_GAME: show_game(); break;
+            case STATE_MENU:
+                show_menu();
+            break;
+            case STATE_RULES:
+                show_rules();
+            break;
+            case STATE_PLAYER_COUNT_SELECTION:
+                show_player_count_selection();
+            break;
+            case STATE_CHARACTER_SELECTION:
+                show_character_selection();
+            break;
+            case STATE_GAME:
+                show_game();
+            break;
         }
 
         blit(buffer, screen, 0, 0, 0, 0, 800, 700);
@@ -28,8 +57,9 @@ int main() {
         rest(20);
     }
 
+    destroy_assets();
     destroy_bitmap(buffer);
+
     return 0;
 }
 END_OF_MAIN();
-
